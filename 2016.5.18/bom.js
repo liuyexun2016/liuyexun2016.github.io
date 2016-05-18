5415d49ba8ac013cfae9c896a19746016fb2505d
liuyexun2016 <392417767@qq.com>
2016-05-18 12:44:46 +0800

    232322

---
+++ 2016.5.18/bom.js
@@ -0,0 +1,32 @@
+var person = {
+    name: 'Mr.loser',
+    gender: 'male',
+    age: 20,
+    sing: function() {
+        console.log('正在唱歌');
+    }
+}
+
+console.log(person.name);
+person.sing();
+
+
+
+for (var 1 in console) {
+    console.log(i + ":" + console[i]);
+}
+console.clear();
+
+console.log(typeof person);
+
+console.log(typeof false);
+console.log(typeof 'false');
+
+consle.log(typeof 100);
+console.log(typeof person.name);
+
+var sites = ['baidu.com', 'sohu.com', 'sina.com.cn', '163.com']
+
+console.log(sites[e]);
+
+console.log(typeof sites);
